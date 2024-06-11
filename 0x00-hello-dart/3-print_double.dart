cf2f0f684e4afd511092d1065ad2044ba268ed62
gumquat <gumquat@gmail.com>
2024-06-11 12:33:06 -0500

    task 1 - 5

---
+++ 0x00-hello-dart/3-print_double.dart
@@ -0,0 +1,6 @@
+import 'dart:core';
+
+void main() {
+  var n = 3.14159265359;
+  print("Double: ${n.toStringAsFixed(2)}");
+}
---
+++ 0x00-hello-dart/5-assertion.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  int score = 85; // Assign the score value here
+  assert(score >= 80, "Uncaught Error: Assertion failed: \"The score must be bigger or equal to 80\"");
+  print("You Passed");
+}
