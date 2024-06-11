82bcd8de54faa2d48184b258922bc4057243d49d
gumquat <gumquat@gmail.com>
2024-06-11 12:52:04 -0500

    test 5

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
-  int score = 85; // Assign the score value here
-  assert(score >= 80, "Uncaught Error: Assertion failed: The score must be bigger or equal to 80');
-  print("You Passed");
+  int nb = int.parse(args[0]);
+  assert(nb >= 80, 'The score must be bigger or equal to 80');
+  print('You Passed');
 }
