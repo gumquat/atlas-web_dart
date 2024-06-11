5191522c5329ebff381a8b2a647740c42df61b52
gumquat <gumquat@gmail.com>
2024-06-11 12:34:05 -0500

    task 6 finished

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  print('Enter a number:');
+  String? input = stdin.readLineSync();
+
+  if (input != null) {
+    int number = int.parse(input);
+
+    if (number > 0) {
+      print('$number is positive');
+    } else if (number == 0) {
+      print('$number is zero');
+    } else {
+      print('$number is negative');
+    }
+  } else {
+    print('Invalid input');
+  }
+}
