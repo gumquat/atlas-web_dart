961790c4fd9cb605eb75f3d3db90743da8fe8d0e
gumquat <gumquat@gmail.com>
2024-06-11 13:50:53 -0500

    fix 4

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,14 +1,13 @@
-// Inner function
-String inner(String name) {
-  List<String> nameParts = name.split(' ');
-  String firstName = nameParts[0];
-  String lastName = nameParts[1];
-  return '$lastName, $firstName';
-}
-
 // Outer function
 void outer(String name, String id) {
   String fullName = inner(name);
   print('Full Name: $fullName, ID: $id');
 }
 
+// Inner function
+String inner(String name) {
+  List<String> nameParts = name.split(' ');
+  String firstName = nameParts[0];
+  String lastName = nameParts[1];
+  return '$lastName $firstName';
+}
