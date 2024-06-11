5fb2d27a2b2dbfb42eff9915ff7f1c3eb5945e97
gumquat <gumquat@gmail.com>
2024-06-11 13:17:07 -0500

    the checker is WRONG

--- 0x00-hello-dart/6-positive_or_negative.dart
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -1,26 +1,20 @@
-void main(List<String> arguments) {
-  if (arguments.length != 1) {
-    print('Please provide a single argument.');
-    return;
-  }
+import 'dart:io';
 
-  int number;
-  try {
-    number = int.parse(arguments[0]);
-  } catch (e) {
-    print('Invalid input. Please provide an integer.');
-    return;
-  }
+void main() {
+  print('Enter a number:');
+  String? input = stdin.readLineSync();
 
-  print('$number is ${getNumberSign(number)}');
-}
+  if (input != null) {
+    int number = int.parse(input);
 
-String getNumberSign(int number) {
-  if (number > 0) {
-    return 'positive';
-  } else if (number == 0) {
-    return 'zero';
+    if (number > 0) {
+      print('$number is positive');
+    } else if (number == 0) {
+      print('$number is zero');
+    } else {
+      print('$number is negative');
+    }
   } else {
-    return 'negative';
+    print('Invalid input');
   }
 }
