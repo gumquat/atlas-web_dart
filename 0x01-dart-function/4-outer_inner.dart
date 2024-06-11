582d3b6c9ac3aa6089d9aded71fcdfce352309d6
gumquat <gumquat@gmail.com>
2024-06-11 13:45:40 -0500

    test 4

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,11 +1,14 @@
-void outer(String name, String id) {
-  String inner() {
-    var names = name.split(' ');
-    var lastNameInitial = names[0];
-    var firstName = names[0];
-
-    return('Hello Agent $lastNameInitial.$firstName your id is $id');
-  }
-
-  print(inner());
+// Inner function
+String inner(String name) {
+  List<String> nameParts = name.split(' ');
+  String firstName = nameParts[0];
+  String lastName = nameParts[1];
+  return '$lastName, $firstName';
 }
+
+// Outer function
+void outer(String name, String id) {
+  String fullName = inner(name);
+  print('Full Name: $fullName, ID: $id');
+}
+
