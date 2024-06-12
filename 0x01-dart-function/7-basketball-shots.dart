b4a1da3169e2e8d8a7b2bb285e6a97c0b8665113
gumquat <gumquat@gmail.com>
2024-06-12 13:03:03 -0500

    test 7

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,6 +1,6 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  int teamAPoints = 0;
-  int teamBPoints = 0;
+  var teamAPoints = 0; // score for Team A 
+  var teamBPoints = 0; // score for taem B
 
   // Calculate team A points here
   teamAPoints += teamA['free_throw'] ?? 0; // 1 point for each free throw
@@ -8,11 +8,11 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   teamAPoints += (teamA['3_pointer'] ?? 0) * 3; // 3 points for each 3-pointer
 
   // Calculate team B points here
-  teamBPoints += teamB['free_throw'] ?? 0; // 1 point for each free throw
-  teamBPoints += (teamB['2_pointer'] ?? 0) * 2; // 2 points for each 2-pointer
-  teamBPoints += (teamB['3_pointer'] ?? 0) * 3; // 3 points for each 3-pointer
+  teamBPoints += teamB['free_throw']! * 1; // 1 point for each free throw
+  teamBPoints += (teamB['2_pointer']! * 2; // 2 points for each 2-pointer
+  teamBPoints += (teamB['3_pointer']! * 3; // 3 points for each 3-pointer
 
-  // Determine the winner here
+  // Determine the winner here with an if else if else
   if (teamAPoints > teamBPoints) {
     return 1; // Team A wins
   } else if (teamAPoints < teamBPoints) {
