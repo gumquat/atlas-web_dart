e9404abaab72f78303dd40aaa658d97d32a6d89f
gumquat <gumquat@gmail.com>
2024-06-12 13:25:05 -0500

    test 10

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,28 +1,18 @@
-function longestPalindrome(s) {
-  // Helper function to check if a string is a palindrome
-  function isPalindrome(str) {
-    return str === str.split('').reverse().join('');
-  }
-
-  // Base case: if input string is less than 3 characters, return 'none'
+String longestPalindrome(String s) {
   if (s.length < 3) {
-    return 'none';
+    return "none";
   }
 
-  let longestPalindrome = '';
+  String longestPalindrome = "";
 
-  // Iterate through all possible substrings
-  for (let i = 0; i < s.length; i++) {
-    for (let j = i + 1; j <= s.length; j++) {
-      const substring = s.slice(i, j);
-
-      // Check if the substring is a palindrome and longer than the current longest palindrome
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 1; j <= s.length; j++) {
+      String substring = s.substring(i, j);
       if (isPalindrome(substring) && substring.length > longestPalindrome.length) {
         longestPalindrome = substring;
       }
     }
   }
 
-  // If no palindrome substring was found, return 'none'
-  return longestPalindrome || 'none';
+  return longestPalindrome.isNotEmpty ? longestPalindrome : "none";
 }
