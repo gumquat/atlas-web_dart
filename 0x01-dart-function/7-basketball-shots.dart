282c0319d905b64bb0cc0865387ec023fc6a4ee4
gumquat <gumquat@gmail.com>
2024-06-12 13:10:27 -0500

    test

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,31 +1,33 @@
-int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  int teamAPoints = 0;
-  int teamBPoints = 0;
+public static int whoWins(Map<String, Integer> teamA, Map<String, Integer> teamB) {
+    int teamAPoints = calculatePoints(teamA);
+    int teamBPoints = calculatePoints(teamB);
 
-  // Calculate team A points
-  int freeThrowA = teamA.containsKey('free_throw') ? teamA['free_throw']! : 0;
-  int twoPointerA = teamA.containsKey('2_pointer') ? teamA['2_pointer']! : 0;
-  int threePointerA = teamA.containsKey('3_pointer') ? teamA['3_pointer']! : 0;
+    if (teamAPoints > teamBPoints) {
+        return 1;
+    } else if (teamAPoints < teamBPoints) {
+        return 2;
+    } else {
+        return 0;
+    }
+}
 
-  teamAPoints += freeThrowA; // 1 point for each free throw
-  teamAPoints += twoPointerA * 2; // 2 points for each 2-pointer
-  teamAPoints += threePointerA * 3; // 3 points for each 3-pointer
+private static int calculatePoints(Map<String, Integer> team) {
+    int points = 0;
+    for (Map.Entry<String, Integer> entry : team.entrySet()) {
+        String shotType = entry.getKey();
+        int shotCount = entry.getValue();
 
-  // Calculate team B points
-  int freeThrowB = teamB.containsKey('free_throw') ? teamB['free_throw']! : 0;
-  int twoPointerB = teamB.containsKey('2_pointer') ? teamB['2_pointer']! : 0;
-  int threePointerB = teamB.containsKey('3_pointer') ? teamB['3_pointer']! : 0;
-
-  teamBPoints += freeThrowB; // 1 point for each free throw
-  teamBPoints += twoPointerB * 2; // 2 points for each 2-pointer
-  teamBPoints += threePointerB * 3; // 3 points for each 3-pointer
-
-  // Determine the winner
-  if (teamAPoints > teamBPoints) {
-    return 1; // Team A wins
-  } else if (teamAPoints < teamBPoints) {
-    return 2; // Team B wins
-  } else {
-    return 0; // Tie
-  }
+        switch (shotType) {
+            case "free_throw":
+                points += shotCount;
+                break;
+            case "2_pointer":
+                points += shotCount * 2;
+                break;
+            case "3_pointer":
+                points += shotCount * 3;
+                break;
+        }
+    }
+    return points;
 }
