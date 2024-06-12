c199070c34cfa3f0acbaa2a62c7a4a1533e8d602
gumquat <gumquat@gmail.com>
2024-06-12 13:17:49 -0500

    test 7 final

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,12 +1,23 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  var teamAPoints = teamA['Free throws']! * 1 + teamA['2 pointers']! * 2 + teamA['3 pointers']! * 3;
-  var teamBPoints = teamB['Free throws']! * 1 + teamB['2 pointers']! * 2 + teamB['3 pointers']! * 3;
+  int teamAPoints = calculateTeamPoints(teamA);
+  int teamBPoints = calculateTeamPoints(teamB);
 
   if (teamAPoints > teamBPoints) {
-    return 1;
+    return 1; // Team A wins
   } else if (teamAPoints < teamBPoints) {
-    return 2;
+    return 2; // Team B wins
   } else {
-    return 0;
+    return 0; // Tie
   }
+}
+
+int calculateTeamPoints(Map<String, int> team) {
+  int points = 0;
+
+  // Safe null access with null-aware operators
+  points += team['Free throws'] ?? 0; // 1 point per free throw
+  points += (team['2 pointers'] ?? 0) * 2; // 2 points per 2-pointer
+  points += (team['3 pointers'] ?? 0) * 3; // 3 points per 3-pointer
+
+  return points;
 }
