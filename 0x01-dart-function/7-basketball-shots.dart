68519de4b6157d48b93b0573ad50db0b5e5f7ab9
gumquat <gumquat@gmail.com>
2024-06-12 13:07:59 -0500

    test

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -4,13 +4,13 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
 
   // Calculate team A points
   teamAPoints += teamA['free_throw'] ?? 1; // 1 point for each free throw
-  teamAPoints += (teamA['2_pointer'] ?? 1) * 2; // 2 points for each 2-pointer
-  teamAPoints += (teamA['3_pointer'] ?? 1) * 3; // 3 points for each 3-pointer
+  teamAPoints += (teamA['2_pointer'] ?? 2); // 2 points for each 2-pointer
+  teamAPoints += (teamA['3_pointer'] ?? 3); // 3 points for each 3-pointer
 
   // Calculate team B points
   teamBPoints += teamB['free_throw'] ?? 1; // 1 point for each free throw
-  teamBPoints += (teamB['2_pointer'] ?? 1) * 2; // 2 points for each 2-pointer
-  teamBPoints += (teamB['3_pointer'] ?? 1) * 3; // 3 points for each 3-pointer
+  teamBPoints += (teamB['2_pointer'] ?? 2); // 2 points for each 2-pointer
+  teamBPoints += (teamB['3_pointer'] ?? 3); // 3 points for each 3-pointer
 
   // Determine the winner
   if (teamAPoints > teamBPoints) {
