395decaae86c63fe91c089fea177c399d18d7bd2
gumquat <gumquat@gmail.com>
2024-06-17 13:36:15 -0500

    task 5

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,31 @@
+class User {
+  String name;
+  int age;
+  double height;
+  int id;
+
+  User({required this.name, required this.age, required this.height, required this.id});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'age': age,
+      'height': height,
+      'id': id,
+    };
+  }
+  // dynamic lets any type of data be used
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'User(id : $id ,name: $name, age: $age, height: $height)';
+  }
+}
---
+++ 0x02-dart-classes/5-main.dart
@@ -0,0 +1,8 @@
+void main() {
+  final djo = User(id: 1, name: "Djo", age: 25, height: 1.89);
+    print(djo.toJson());
+  Map map = {'id': 3, 'name': 'Youssef', 'age': 26, 'height': 1.9};
+  final youssef = User.fromJson(map);
+  print(djo.toString());
+  print(youssef.toString());
+}
