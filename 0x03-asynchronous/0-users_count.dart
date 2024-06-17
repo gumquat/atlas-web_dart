65b498af690509677c2aa603ae7dd2b105c7b54a
gumquat <gumquat@gmail.com>
2024-06-17 14:40:51 -0500

    task 0

---
+++ 0x03-asynchronous/0-users_count.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+Future<void> usersCount() async {
+  try {
+    final userCount = await fetchUsersCount();
+    print('Number of users: $userCount');
+  } catch (e) {
+    print('Error fetching user count: $e');
+  }
+}
+
+// Simulated fetchUsersCount() function
+Future<int> fetchUsersCount() {
+  return Future.delayed(const Duration(seconds: 2), () => 42);
+}
