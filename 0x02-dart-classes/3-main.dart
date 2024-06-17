0245e18e616d42b9fd011114683ef07652fd0815
gumquat <gumquat@gmail.com>
2024-06-17 13:24:25 -0500

    task 3

---
+++ 0x02-dart-classes/3-main.dart
@@ -0,0 +1,8 @@
+void main() {
+  final ps = Password(password: "Passwordcode");
+  print(ps.toString());
+  print(ps.isValid());
+  final ps2 = Password(password: "PasswordDecode3");
+  print(ps2.toString());
+  print(ps2.isValid());
+}
